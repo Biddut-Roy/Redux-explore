8415fdb55a81d2ed908364f9eaf5bab487755026
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-08 23:16:56 +0600

    Recheck and data update

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
-const { createStore } = require("redux")
-
+// plan- 
+// State- store 0 .{array , object or any type}
+// action- increment , Decrement and Reset . {array function, return data {} under type , payload}
+// reducer- array function 2 argument state and action - under switch condition case-1 (any-action set) to return {} and Default state show.
+// create-Store- show data store variable.subscribe( under array function under console.log(store variable.getState()))
+//  store data action to reducer condition data store set. store variable.dispatch(create_action())
 
 // state
 
+const { createStore } = require("redux")
 
-const initialCounterState = {
-    Count: 0
-}
-const initialUserState = {
-    users: [
-        {name: 'John', email: 'tiskule@gmail.com'}
-    ]
+const INCREMENT  = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const initialStateCounter = {
+    count: 0
 }
 
 //  action 
 
-const incrementCount = () =>{
-    return{
-        type:'INCREMENT',
+const increment = () => {
+    return {
+        type:INCREMENT
     }
 }
 
-const decrementCount = () =>{
+const Decrement = () => {
     return{
-        type:'DECREMENT',
+        type: DECREMENT
     }
 }
-const UserUpdate = () =>{
+const Reset = () => {
     return{
-        type:'user_update',
-        payload:   {name: 'John sing', email: 'tiskule23voum@gmail.com'}
+        type: RESET
     }
 }
+
+// const UserUpdate = () => {
+//     return {
+//         type: 'user_update',
+//         payload: { name: 'John sing', email: 'tiskule23voum@gmail.com' }
+//     }
+// }
 
 //  reducer 
-const updateReducer = (state=initialCounterState , action) =>{
+const reCheckDataSlices = (state = initialStateCounter , action) => {
     switch (action.type) {
-        case 'INCREMENT':
-            return{
+        case increment :
+            return {
                 ...state,
-                Count: action.Count + 1
+                count: action.count + 1
             }
-            case 'DECREMENT':
-            return{
+        case Decrement :
+            return {
                 ...state,
-                Count: action.Count + 1
+                count: action.count - 1
             }
-
+        case Decrement :
+            return {
+                ...state,
+                count: 0
+            }
+    
         default:
-           state;
+            state
     }
 }
 
+
+
 // store create
-
-const store = createStore(updateReducer)
- store.subscribe(()=>{
+const store = createStore(reCheckDataSlices)
+store.subscribe(()=>{
     console.log(store.getState());
- })
-
- store.dispatch(incrementCount())
+})
+ store.dispatch(increment())
+ store.dispatch(increment())
+ store.dispatch(Decrement())
